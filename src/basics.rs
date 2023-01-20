5f9cede8ec7bcc845323ab06126c933a37ecd516
archnim <archnim01@gmail.com>
2023-01-20 09:06:34 +0000

    little patch on error printing

--- src/basics.rs
+++ src/basics.rs
@@ -81,64 +81,64 @@ pub fn magenta_out(s: String) {
 
 #[allow(dead_code)]
 pub fn white_err(s: String) {
-	print!("\x1b[1m");
-	print!("\x1b[37m");
-	println!("{}", s);
-	print!("\x1b[0m");
+	eprint!("\x1b[1m");
+	eprint!("\x1b[37m");
+	eprintln!("{}", s);
+	eprint!("\x1b[0m");
 }
 
 #[allow(dead_code)]
 pub fn black_err(s: String) {
-	print!("\x1b[1m");
-	print!("\x1b[30m");
-	println!("{}", s);
-	print!("\x1b[0m");
+	eprint!("\x1b[1m");
+	eprint!("\x1b[30m");
+	eprintln!("{}", s);
+	eprint!("\x1b[0m");
 }
 
 #[allow(dead_code)]
 pub fn blue_err(s: String) {
-	print!("\x1b[1m");
-	print!("\x1b[34m");
-	println!("{}", s);
-	print!("\x1b[0m");
+	eprint!("\x1b[1m");
+	eprint!("\x1b[34m");
+	eprintln!("{}", s);
+	eprint!("\x1b[0m");
 }
 
 #[allow(dead_code)]
 pub fn red_err(s: String) {
-	print!("\x1b[1m");
-	print!("\x1b[31m");
-	println!("{}", s);
-	print!("\x1b[0m");
+	eprint!("\x1b[1m");
+	eprint!("\x1b[31m");
+	eprintln!("{}", s);
+	eprint!("\x1b[0m");
 }
 
 #[allow(dead_code)]
 pub fn green_err(s: String) {
-	print!("\x1b[1m");
-	print!("\x1b[32m");
-	println!("{}", s);
-	print!("\x1b[0m");
+	eprint!("\x1b[1m");
+	eprint!("\x1b[32m");
+	eprintln!("{}", s);
+	eprint!("\x1b[0m");
 }
 
 #[allow(dead_code)]
 pub fn yellow_err(s: String) {
-	print!("\x1b[1m");
-	print!("\x1b[33m");
-	println!("{}", s);
-	print!("\x1b[0m");
+	eprint!("\x1b[1m");
+	eprint!("\x1b[33m");
+	eprintln!("{}", s);
+	eprint!("\x1b[0m");
 }
 
 #[allow(dead_code)]
 pub fn cyan_err(s: String) {
-	print!("\x1b[1m");
-	print!("\x1b[36m");
-	println!("{}", s);
-	print!("\x1b[0m");
+	eprint!("\x1b[1m");
+	eprint!("\x1b[36m");
+	eprintln!("{}", s);
+	eprint!("\x1b[0m");
 }
 
 #[allow(dead_code)]
 pub fn magenta_err(s: String) {
-	print!("\x1b[1m");
-	print!("\x1b[35m");
-	println!("{}", s);
-	print!("\x1b[0m");
+	eprint!("\x1b[1m");
+	eprint!("\x1b[35m");
+	eprintln!("{}", s);
+	eprint!("\x1b[0m");
 }
